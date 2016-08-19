6ead088e4beeae1756169236f4b549822300763f
Karpenko <_slavka@bigmir.net>
2016-08-19 23:19:52 +0300

    V1

---
+++ www/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Скоро вихід</title>
+	<link rel="stylesheet" type="text/css"  href="startStyle.css">
+</head>
+<body>
+<div class="startStyle">
+	<H3> Вітаю, Відвідувачу!!!</H3>
+	<p>Даний сайт на стадії розробки. Скоро вийде дуже чорнова його версія. </p>
+	
+	<br>
+		<a href="http://www.goals2go.com/wp-content/uploads/site-under-construction.jpg">Джерело зображення</a>
+	<p>Автор Вячеслав Карпенко 2016р.</p>
+</div>
+</body>
+</html>
+
+
+
